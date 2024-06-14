fa8647edd2540a03102ca0f846a58a99c6facdaa
Mist Uber <luciamistuber@gmail.com>
2024-06-14 17:59:37 +0200

    added btns

--- calculatorTwo.py
+++ calculatorTwo.py
@@ -3,83 +3,92 @@ from tkinter.ttk import *
 
 window= Tk()
 pad=[
-  {
+ [{
      "text":"CE",
-     type:"func",
+     "type":"func",
      "width":3,
   },{
      "text":"C",
-     type:"func",
+     "type":"func",
      "width":3,
   },{
      "text":"DEL",
-     type:"func",
+      "type":"func",
      "width":3,
   },{
      "text":"/",
-     type:"func",
+     "type":"func",
      "width":3,
-  },{
+  }] ,
+  [{
      "text":"7",
      "width":3,
-     type:"num",
+     "type":"num",
   },{
      "text":"8",
      "width":3,
-     type:"num",
+     "type":"num",
   },{
      "text":"9",
      "width":3,
-     type:"num",
+     "type":"num",
   },{
      "text":"X",
      "width":3,
-     type:"op",
-  },{
+     "type":"op",
+  }],[{
      "text":"4",
-     type:"num",
+     "type":"num",
      "width":3,
   },{
      "text":"5",
-     type:"num",
+     "type":"num",
      "width":3,
   },{
      "text":"6",
-     type:"num",
+     "type":"num",
      "width":3,
   },{
     "text":"-",
-    type:"op",
+    "type":"op",
     "width":3,
-  },{
+  }],[{
      "text":"1",
-     type:"num",
+     "type":"num",
      "width":3,
   },{
      "text":"2",
-     type:"num",
+     "type":"num",
      "width":3,
   },{
      "text":"3",
-     type:"num",
+     "type":"num",
      "width":3,
   },{
     "text":"+",
-    type:"op",
+    "type":"op",
     "width":3,
-  },{
+  }],[{
      "text":",",
-     type:"num",
+     "type":"num",
      "width":3,
   },{
      "text":"0",
-     type:"num",
+     "type":"num",
      "width":3,
   },{
     "text":"=",
-    type:"op",
+    "type":"op",
     "width":6,
-  }
+  }]
 ]
+
+def handleOps(row, column, action_kind):
+    pass
+
+for row in range(len(pad)):
+    for col in range(len(pad[row])):
+        btn= Button(text=pad[row][col]['text'], width=pad[row][col]["width"],  command=lambda row=row, column=col, action_kind=pad[row][col]["type"]:handleOps(row, column, action_kind))
+        btn.pack()
 
 window.mainloop()
