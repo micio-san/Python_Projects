35f23358a11fda0a95fcc07fcd6255ed28cac6c4
Mist Uber <106929734+micio-san@users.noreply.github.com>
2024-06-26 17:36:51 +0200

    Add files via upload

---
+++ calculatorTwo.py
@@ -0,0 +1,165 @@
+from tkinter import * 
+from tkinter.ttk import *
+
+window= Tk()
+frame = Frame(window)
+window.geometry("300x300")
+#write first in sec val and when op is called move to second
+
+calculator_screen= StringVar(window)
+
+valueOne=""
+labelOne= Label(textvariable=calculator_screen,  font=('consolas',20))
+labelOne.grid(row=1, column=3)
+
+valueTwo=""
+calculator_screen_sm= StringVar(window)
+labelTwo= Label(textvariable=calculator_screen_sm)
+showLabelTwo = 0 if len(valueTwo)==0 else 3
+labelTwo.grid(row=0, column=showLabelTwo)
+evaluated=FALSE
+
+pad=[
+ [{
+     "text":"CE",
+     "type":"func",
+     "width":10,
+  },{
+     "text":"C",
+     "type":"func",
+     "width":10,
+  },{
+     "text":"DEL",
+      "type":"func",
+     "width":10,
+  },{
+     "text":"/",
+     "type":"func",
+     "width":10,
+  }] ,
+  [{
+     "text":"7",
+     "width":10,
+     "type":"num",
+  },{
+     "text":"8",
+     "width":10,
+     "type":"num",
+  },{
+     "text":"9",
+     "width":10,
+     "type":"num",
+  },{
+     "text":"*",
+     "width":10,
+     "type":"op",
+  }],[{
+     "text":"4",
+     "type":"num",
+     "width":10,
+  },{
+     "text":"5",
+     "type":"num",
+     "width":10,
+  },{
+     "text":"6",
+     "type":"num",
+     "width":10,
+  },{
+    "text":"-",
+    "type":"op",
+    "width":10,
+  }],[{
+     "text":"1",
+     "type":"num",
+     "width":10,
+  },{
+     "text":"2",
+     "type":"num",
+     "width":10,
+  },{
+     "text":"3",
+     "type":"num",
+     "width":10,
+  },{
+    "text":"+",
+    "type":"op",
+    "width":10,
+  }],[{
+     "text":",",
+     "type":"num",
+     "width":10,
+  },{
+     "text":"0",
+     "type":"num",
+     "width":10,
+  },{
+    "text":"=",
+    "type":"func",
+    "width":10,
+  }]
+]
+
+def showValues(value, displayer): {
+  displayer.set(value)
+}
+
+def handleFunc(row, column):
+   global valueOne
+   global valueTwo
+   global calculator_screen
+   global calculator_screen_sm
+   global evaluated
+   match pad[row][column]["text"]:
+       case "=":
+        if not evaluated:
+         valueTwo += valueOne
+         valueOne=eval(valueTwo)
+         showValues(valueOne, calculator_screen)
+         showValues(valueTwo, calculator_screen_sm)
+         evaluated=TRUE
+        else:
+         return
+       case "CE":
+         valueOne=""
+         valueTwo=""
+         evaluated=FALSE
+         showValues(valueOne, calculator_screen)
+         showValues(valueTwo, calculator_screen_sm)
+       case "C":
+         valueOne=""
+         showValues(valueOne, calculator_screen)
+         
+        
+def handleOps(row, column, action_kind):
+    global valueOne
+    global valueTwo
+    global calculator_screen
+    global calculator_screen_sm
+    match action_kind:
+        case "num":
+            if not evaluated:
+             valueOne += pad[row][column]["text"]
+             calculator_screen.set(valueOne)
+            else:
+               return
+        case "op":
+            if len(valueTwo) == 0 and not evaluated:
+             valueOne+=pad[row][column]["text"]
+             valueTwo=valueOne
+             calculator_screen_sm.set(valueTwo)
+             valueOne=""
+             calculator_screen.set(valueOne)
+            else:
+              return
+        case "func":
+            handleFunc(row, column)
+        
+        
+
+for row in range(len(pad)):
+    for col in range(len(pad[row])):
+        pad[row][col] = Button(text=pad[row][col]['text'], width=pad[row][col]["width"], command=lambda row=row, column=col, action_kind=pad[row][col]["type"]:handleOps(row, column, action_kind))
+        pad[row][col].grid(row=row+2, column=col)
+
+window.mainloop()
