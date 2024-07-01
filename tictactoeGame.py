4b7d032b270665d040eebfdd52a3e1deb5e60345
Mist Uber <luciamistuber@gmail.com>
2024-07-01 12:53:46 +0200

    adding Tutorial tic-tac-toe

---
+++ tictactoeGame.py
@@ -0,0 +1,26 @@
+class TicTacToe:
+    def __init__(self) -> None:
+        # the _ indicates a temporary or throwaway variable
+        self.board=[" " for _ in range(9)]
+        self.currentWinner=None
+
+    def print_board(self):
+       for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+           print('| ' + ' | '.join(row) + " |")
+
+    @staticmethod
+    def print_board_nums():
+     number_board=[[str(i) for i in range(j*3,(j+1)*3)] for j in range(3)]
+     for row in number_board:
+        for cell in row:
+           print(cell)
+
+    def available_moves(self):
+       moves=[]
+       #enumerate in python creates a list of tuples with [index, value] 
+       # for(i, spot) in enumerate(self.board):
+       #    if spot == ' ' :
+       #       moves.append(i)
+       # return moves
+       #alternative syntax wirh list comprehension
+       return [i for i, spot in enumerate(self.board) if spot == '']
---
+++ tictactoePlayer.py
@@ -0,0 +1,33 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        self.letter=letter
+    def get_move(self, game):
+        pass
+
+class RandomPcPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square= random.choice(game.available_moves())
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square=False
+        val=None
+        while not valid_square:
+            square=input(self.letter + "'s turn" )
+            try:
+               val=int(square)
+               if val not in game.available_moves():
+                   raise ValueError
+               valid_square=True
+            except ValueError:
+                print("Wrong try again")
+            return val
