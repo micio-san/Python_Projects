87332e0a82a9c526df1fe8424a1740ae59df8277
Mist Uber <106929734+micio-san@users.noreply.github.com>
2024-07-09 14:42:05 +0200

    Update tictactoeGameMine.py

--- tictactoeGameMine.py
+++ tictactoeGameMine.py
@@ -1,7 +1,9 @@
-#game loop
-  #check player
-  #check if empty || occupied
-  #check 
+#drawboard empy, make array
+#game loop while !not_ended:
+  # check player => askInput
+  # check if empty || occupied
+  # check if winner with combos= game end || game goes =>  drawboard => switch player askInput
+
 
 # if __ == "__main__"
    #startGame()
