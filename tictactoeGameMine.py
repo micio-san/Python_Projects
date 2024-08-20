2ef6e53c58674141452e20e41463412c2ab36e53
Mist Uber <106929734+micio-san@users.noreply.github.com>
2024-08-20 16:09:00 +0200

    Delete tictactoeGameMine.py

--- tictactoeGameMine.py
+++
@@ -1,9 +0,0 @@
-#drawboard empy, make array
-#game loop while !not_ended:
-  # check player => askInput
-  # check if empty || occupied
-  # check if winner with combos= game end || game goes =>  drawboard => switch player askInput
-
-
-# if __ == "__main__"
-   #startGame()
